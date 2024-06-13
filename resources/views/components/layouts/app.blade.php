ac4026e40b84966cf017edacbcc7daa042b845e1
Safie Misri <safie19@gmail.com>
2024-06-13 17:05:30 +0800

    update Theme

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html data-theme="wireframe" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html data-theme="winter" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
         extend: {},
     },
     daisyui: {
-    themes: ["light", "dark", "lofi", "wireframe"],
+    themes: ["light", "dark", "lofi", "wireframe", "winter"],
   },
 
     // Add daisyUI
